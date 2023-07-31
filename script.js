4ef7aa781bba95c95c399ee3da70a1bd676dd91a
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-07-31 23:17:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+// script.js
+
 function highlight() {
-    //Write your code here
-
-
+    let strongTags = document.getElementsByTagName("strong");
+    for (let i = 0; i < strongTags.length; i++) {
+        strongTags[i].style.color = "green";
+    }
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+    let strongTags = document.getElementsByTagName("strong");
+    for (let i = 0; i < strongTags.length; i++) {
+        strongTags[i].style.color = "black";
+    }
 }
